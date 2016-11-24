a48c7a3409ad29191f32de6965153f42ab5c2cfe
thierabsy <thierabsy@yahoo.fr>
2016-11-24 23:28:41 +0000

    LINKS

--- js/myjs.js
+++ js/myjs.js
@@ -11,11 +11,21 @@ $(document).ready(function() {
 });
 $('#myModal').modal();
 
-$('.collapse').collapse('toggle');
+// $('.collapse').collapse('toggle');
 
 $('.bienvenue').typer(['BIENVENUE AU CASR 3PM ACADEMY']);
 
 // ASIDE CAROUSEL
+// Javascript to enable link to tab
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+} 
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+});
 
 
 // OWL
